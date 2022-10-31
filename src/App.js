9dccf8a4d486300302c5fc6935d6d982f00b9964
Venus Ang <venus@users.noreply.github.com>
2022-10-31 15:54:24 -0700

    defined state values and hooked in API to useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react';
 function App() {
 
+  let [artId, setArtId] = useState(12720)
+  let [data, setData] = useState({})
+
   useEffect(() => {
-    document.title = 'Welcome to Art World!';
-  });
+    document.title = 'Welcome to Artworld'
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${artId}`)
+      .then(response => response.json())
+      .then(resData => setData(resData))
+  }, [artId]);
+
 
   return (
     <div className="App">
